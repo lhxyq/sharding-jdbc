62fa4b550c755beff52eb8a23ada5351e033306d
lhxyq <2446059046@qq.com>
2017-08-29 11:46:37 +0800

    init program

---
+++ src/main/java/shard/shardingjdbc/demo/ShardingJdbcApplication.java
@@ -0,0 +1,12 @@
+package shard.shardingjdbc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShardingJdbcApplication.class, args);
+	}
+}
